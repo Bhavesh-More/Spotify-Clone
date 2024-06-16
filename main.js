dd488d79fa48044bc98a8fe441b822ee33728cae
Bhavesh-More <bhaveshmmore2006@gmail.com>
2024-06-16 15:33:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+let  Folders_paths = []; // Consider if this is necessary
+// let songs_folders_paths = []; // Consider if this is necessary
+// let imgs_folders_paths = []; // Consider if this is necessary
+
+function playlists_collector(playlist_name, playlist_img, playlist_maker, song_path , imgs_path) {
+    playlist_adder(playlist_name, playlist_img, playlist_maker);
+    path_collector( song_path , imgs_path);
+
+}
+
+function path_collector( song_path , imgs_path) {
+    Folders_paths.push({ pathofsong: `${ song_path}` ,
+                         pathofimg : `${ imgs_path}`  
+                       } );
+}
+
+
+
+
+
+playlists_collector("Myplaylist","song img/ainmal.png","Artists","http://127.0.0.1:3000/playlists/My%20playlist/songs/","http://127.0.0.1:3000/playlists/My%20playlist/imgss/");
+playlists_collector("All time","song img/world of war.png","papa","http://127.0.0.1:3000/playlists/All%20time/songs/","http://127.0.0.1:3000/playlists/All%20time/imgss/");
+playlists_collector("Boom","playlists/playlist img/Boom.png","Bhavesh","http://127.0.0.1:3000/playlists/Boom/songs/","http://127.0.0.1:3000/playlists/Boom/imgss/");

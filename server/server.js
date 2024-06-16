91162b3f7af6a96a7851419c3eec42cb48795989
Bhavesh-More <bhaveshmmore2006@gmail.com>
2024-06-16 20:51:39 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const { subscribe } = require("diagnostics_channel");
+const express = require("express");
+const app = express();
+const cors = require('cors');
+
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
+
+
+app.get("/", (req, res) =>{
+    res.json({ name: "Code Bless You", subscribe: true});
+
+});
+
+app.listen(5000, () => console.log("server is listening on 5000"));

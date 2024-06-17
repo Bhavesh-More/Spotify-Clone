b818ce432f88c1dd0ab98db71e7a66f791f32166
Bhavesh-More <bhaveshmmore2006@gmail.com>
2024-06-17 16:22:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,27 @@
-let Folders_paths = [];
 
-function playlists_collector(playlist_name, playlist_img, playlist_maker, song_path, imgs_path) {
+let  Folders_paths = []; // Consider if this is necessary
+// let songs_folders_paths = []; // Consider if this is necessary
+// let imgs_folders_paths = []; // Consider if this is necessary
+
+function playlists_collector(playlist_name, playlist_img, playlist_maker, song_path , imgs_path) {
     playlist_adder(playlist_name, playlist_img, playlist_maker);
-    path_collector(song_path, imgs_path);
+    path_collector( song_path , imgs_path);
+
 }
 
-function path_collector(song_path, imgs_path) {
-    Folders_paths.push({
-        pathofsong: `${song_path}`,
-        pathofimg: `${imgs_path}`
-    });
+function path_collector( song_path , imgs_path) {
+    Folders_paths.push({ pathofsong: `${ song_path}` ,
+                         pathofimg : `${ imgs_path}`  
+                       } );
 }
 
-// Use raw.githubusercontent.com for GitHub URLs
-playlists_collector(
-    "Animal",
-    "https://raw.githubusercontent.com/Bhavesh-More/Spotify-Clone/main/play/imgss/animal.png",
-    "Artists",
-    "https://raw.githubusercontent.com/Bhavesh-More/Spotify-Clone/main/play/big/",
-    "https://raw.githubusercontent.com/Bhavesh-More/Spotify-Clone/main/play/imgss/"
-);
-playlists_collector(
-    "All time",
-    "https://raw.githubusercontent.com/Bhavesh-More/Spotify-Clone/main/playlists/All%20time/imgss/world%20of%20war.png",
-    "papa",
-    "https://raw.githubusercontent.com/Bhavesh-More/Spotify-Clone/main/playlists/All%20time/songs/",
-    "https://raw.githubusercontent.com/Bhavesh-More/Spotify-Clone/main/playlists/All%20time/imgss/"
-);
-playlists_collector(
-    "Boom",
-    "https://raw.githubusercontent.com/Bhavesh-More/Spotify-Clone/main/playlists/Boom/imgss/Boom.png",
-    "Bhavesh",
-    "https://raw.githubusercontent.com/Bhavesh-More/Spotify-Clone/main/playlists/Boom/songs/",
-    "https://raw.githubusercontent.com/Bhavesh-More/Spotify-Clone/main/playlists/Boom/imgss/"
-);
+
+
+
+
+playlists_collector("Animal","song img/ainmal.png","Artists","http://127.0.0.1:3000/play/big/","http://127.0.0.1:3000/play/imgss/");
+playlists_collector("All time","song img/world of war.png","papa","https://github.com/Bhavesh-More/Spotify-Clone/blob/main/playlists/All%20time/songs/","https://github.com/Bhavesh-More/Spotify-Clone/blob/main/playlists/All%20time/imgss/");
+playlists_collector("Boom","playlists/playlist img/Boom.png","Bhavesh","http://127.0.0.1:3000/playlists/Boom/songs/","http://127.0.0.1:3000/playlists/Boom/imgss/");
+
+
+
